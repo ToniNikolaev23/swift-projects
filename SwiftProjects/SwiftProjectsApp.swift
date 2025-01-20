04624e2a34c19ee22ddb2686d8e7f2d630e7cff6
Toni Stoyanov <toni.n.stoyanov@icloud.com>
2025-01-20 15:22:41 +0200

    Initial Commit

---
+++ SwiftProjects/SwiftProjectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftProjectsApp.swift
+//  SwiftProjects
+//
+//  Created by Toni Stoyanov on 20.01.25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftProjectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
