a8fb923da1d08915a2eb3c471718d4aeb16af9bf
Toni Stoyanov <toni.n.stoyanov@icloud.com>
2025-01-21 21:33:09 +0200

    Add Colors and create Home View for Spotify

---
+++ SwiftProjects/Spotify/Components/SpotifyCategoryCell.swift
@@ -0,0 +1,35 @@
+//
+//  SpotifyCategoryCell.swift
+//  SwiftProjects
+//
+//  Created by Toni Stoyanov on 21.01.25.
+//
+
+import SwiftUI
+
+struct SpotifyCategoryCell: View {
+    var title: String = "All"
+    var isSelected: Bool = false
+    
+    var body: some View {
+        Text(title)
+            .font(.callout)
+            .frame(minWidth: 35)
+            .padding(.vertical, 8)
+            .padding(.horizontal, 10)
+            .background(isSelected ? .spotifyGreen : .spotifyDarkGray)
+            .foregroundStyle(isSelected ? .spotifyBlack : .spotifyWhite)
+            .cornerRadius(16)
+    }
+}
+
+#Preview {
+    ZStack {
+        Color.black.ignoresSafeArea()
+        VStack {
+            SpotifyCategoryCell(title: "Title goes here")
+            SpotifyCategoryCell(title: "Title goes here", isSelected: true)
+            SpotifyCategoryCell(isSelected: false)
+        }
+    }
+}
---
+++ SwiftProjects/Spotify/Models/Category.swift
@@ -0,0 +1,12 @@
+//
+//  Category.swift
+//  SwiftProjects
+//
+//  Created by Toni Stoyanov on 21.01.25.
+//
+
+import Foundation
+
+enum Category: String, CaseIterable {
+    case all, music, podcasts, audiobooks
+}
