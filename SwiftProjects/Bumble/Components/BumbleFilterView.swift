91d2f0d19707cb32e9df3cd1d83a41701349b55e
Toni Stoyanov <toni.n.stoyanov@icloud.com>
2025-01-25 15:00:37 +0200

    Init Bumble App, add colors, create navigation menu

---
+++ SwiftProjects/Bumble/Components/BumbleFilterView.swift
@@ -0,0 +1,56 @@
+//
+//  BumbleFilterView.swift
+//  SwiftProjects
+//
+//  Created by Toni Stoyanov on 25.01.25.
+//
+
+import SwiftUI
+
+struct BumbleFilterView: View {
+    var options: [String] = ["Everyone", "Tranding"]
+    @Binding var selection: String
+    @Namespace private var namespace
+    
+    var body: some View {
+        HStack(alignment: .top, spacing: 32) {
+            ForEach(options, id: \.self) { option in
+                VStack(spacing: 8) {
+                    Text(option)
+                        .frame(maxWidth: .infinity)
+                        .font(.subheadline)
+                        .fontWeight(.medium)
+                    
+                    if selection == option {
+                        RoundedRectangle(cornerRadius: 2)
+                            .frame(height: 1.5)
+                            .matchedGeometryEffect(id: "selection", in: namespace)
+                    }
+                   
+                }
+                .padding(.top, 8)
+                .background(Color.black.opacity(0.001))
+                .foregroundStyle(selection == option ? .bumbleBlack : .bumbleGray)
+                .onTapGesture {
+                    selection = option
+                }
+            }
+            
+        }
+        .animation(.smooth, value: selection)
+    }
+}
+
+fileprivate struct BumbleFilterViewPreview: View {
+    var options: [String] = ["Everyone", "Tranding"]
+    @State private var selection = "Everyone"
+    
+    var body: some View {
+        BumbleFilterView(options: options, selection: $selection)
+    }
+}
+
+#Preview {
+    BumbleFilterViewPreview()
+        .padding()
+}
